f0a0cbe8da37b953a56a20a323cb558d97edbd6c
Harsh2460 <sharshvardhan2006@gmail.com>
2025-09-06 12:34:35 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "dhun": "https://www.youtube.com/watch?v=cUmUOb7j3dc",
+    "fear": "https://www.youtube.com/watch?v=D6MOuX980gc",
+    "powerhouse": "https://www.youtube.com/watch?v=OXHTlMPbX7o",
+    "sulthan": "https://www.youtube.com/watch?v=PWaPCqeCfeY"
+}
